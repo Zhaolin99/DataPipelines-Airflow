141efde727b0e03139fab1439cb448829554234f
Zhaolin99 <78530943+Zhaolin99@users.noreply.github.com>
2023-12-09 12:49:15 -0800

    Update final_project_sql_statements.py

--- final_project_sql_statements.py
+++ final_project_sql_statements.py
@@ -1,7 +1,8 @@
-songplay_table_insert = ("""
-    SELECT
-        md5(events.sessionid || events.start_time) songplay_id,
-        events.start_time, 
+class SqlQueries:
+    songplay_table_insert = ("""
+        SELECT
+                md5(events.sessionid || events.start_time) songplay_id,
+                events.start_time, 
                 events.userid, 
                 events.level, 
                 songs.song_id, 
@@ -18,62 +19,63 @@ songplay_table_insert = ("""
                 AND events.length = songs.duration
     """)
 
-user_table_insert = ("""
+    user_table_insert = ("""
         SELECT distinct userid, firstname, lastname, gender, level
         FROM staging_events
         WHERE page='NextSong'
     """)
 
-song_table_insert = ("""
+    song_table_insert = ("""
         SELECT distinct song_id, title, artist_id, year, duration
         FROM staging_songs
     """)
 
-artist_table_insert = ("""
+    artist_table_insert = ("""
         SELECT distinct artist_id, artist_name, artist_location, artist_latitude, artist_longitude
         FROM staging_songs
     """)
 
-time_table_insert = ("""
+    time_table_insert = ("""
         SELECT start_time, extract(hour from start_time), extract(day from start_time), extract(week from start_time), 
                extract(month from start_time), extract(year from start_time), extract(dayofweek from start_time)
         FROM songplays
     """)
 
 class create_tables:
+    create_staging_events = ("""
+        CREATE TABLE IF NOT EXISTS public.staging_events (
+            artist varchar(256),
+            auth varchar(256),
+            firstname varchar(256),
+            gender varchar(256),
+            iteminsession int4,
+            lastname varchar(256),
+            length numeric(18,0),
+            "level" varchar(256),
+            location varchar(256),
+            "method" varchar(256),
+            page varchar(256),
+            registration numeric(18,0),
+            sessionid int4,
+            song varchar(256),
+            status int4,
+            ts timestamp,
+            useragent varchar(256),
+            userid int4
+        );
+    """)
   
-  CREATE TABLE IF NOT EXISTS public.staging_events (
-  	artist varchar(256),
-  	auth varchar(256),
-  	firstname varchar(256),
-  	gender varchar(256),
-  	iteminsession int4,
-  	lastname varchar(256),
-  	length numeric(18,0),
-  	"level" varchar(256),
-  	location varchar(256),
-  	"method" varchar(256),
-  	page varchar(256),
-  	registration numeric(18,0),
-  	sessionid int4,
-  	song varchar(256),
-  	status int4,
-  	ts timestamp,
-  	useragent varchar(256),
-  	userid int4
-  );
-  
-  CREATE TABLE IF NOT EXISTS public.staging_songs (
-  	num_songs int4,
-  	artist_id varchar(256),
-  	artist_name varchar(256),
-  	artist_latitude numeric(18,0),
-  	artist_longitude numeric(18,0),
-  	artist_location varchar(256),
-  	song_id varchar(256),
-  	title varchar(256),
-  	duration numeric(18,0),
-  	"year" int4
-  );
-  
-  
+    create_staging_songs = ("""
+        CREATE TABLE IF NOT EXISTS public.staging_songs (
+            num_songs int4,
+            artist_id varchar(256),
+            artist_name varchar(256),
+            artist_latitude numeric(18,0),
+            artist_longitude numeric(18,0),
+            artist_location varchar(256),
+            song_id varchar(256),
+            title varchar(256),
+            duration numeric(18,0),
+            "year" int4
+        );
+    """)
